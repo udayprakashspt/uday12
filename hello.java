f2629c16fb92ee41e25747246a25c20337fcbce2
udayprakashspt <129489345+udayprakashspt@users.noreply.github.com>
2023-04-27 16:14:41 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ class hello
    public static void main(String[]args)
     {
 	 System.out.println("Hello,world");
-         System.out.println("Hello,world");
+         System.out.println("Uday kumar");
     }
 }
